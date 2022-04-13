3ae2df8b1c851efc8b9289abf8e448a0589dff86
Dennis Schmalacker <dennis-on-github@dclabs.de>
2022-04-13 13:52:18 +0200

    Improve client

--- ddnsclient/main.py
+++ ddnsclient/main.py
@@ -1,6 +1,8 @@
 import logging
 import signal
 import sys
+from dataclasses import dataclass
+from ipaddress import IPv4Address, IPv6Address, AddressValueError
 from threading import Event
 
 import click
@@ -26,6 +28,27 @@ SIGNAL_MAP = {
 }
 
 
+@dataclass
+class IPRegistry:
+    ipv4: IPv4Address = IPv4Address("0.0.0.0")
+    ipv4_changed: bool = False
+
+    ipv6: IPv6Address = IPv6Address("::")
+    ipv6_changed: bool = False
+
+    def set_ipv4(self, ipv4):
+        ipv4 = IPv4Address(ipv4)
+        self.ipv4_changed = self.ipv4 != ipv4
+        logger.info(f"IPv4 has {'not' if self.ipv4_changed else ''} changed: {self.ipv4} -> {ipv4}")
+        self.ipv4 = ipv4
+
+    def set_ipv6(self, ipv6):
+        ipv6 = IPv6Address(ipv6)
+        self.ipv6_changed = self.ipv6 != ipv6
+        logger.info(f"IPv6 has {'not' if self.ipv6_changed else ''} changed: {self.ipv6} -> {ipv6}")
+        self.ipv6 = ipv6
+
+
 @click.command()
 @click.option("-s", "--ddns-server")
 @click.option("-u", "--login")
@@ -33,63 +56,58 @@ SIGNAL_MAP = {
 @click.option("-d", "--delay", type=int, default=600, show_default=True)
 @click.option("-w", "--web")
 @click.option("-w6", "--web-v6")
+@click.option("-n", "--dry-run")
 @click.option("-v", "--debug", is_flag=True)
 @click_config_file.configuration_option()
-def command(ddns_server, login, password, delay, web, web_v6=None, debug=False):
+def command(ddns_server, login, password, delay, web, web_v6=None, dry_run=False, debug=False):
     if debug:
         logger.setLevel(logging.DEBUG)
     logger.info(f"Starting ddnsclient version {ddnsclient.VERSION}")
     logger.debug(f"Config: {locals()}")
 
-    last_ipv4 = None
-    last_ipv6 = None
+    registry = IPRegistry()
 
     with daemon.DaemonContext(stdout=sys.stdout, stderr=sys.stderr, signal_map=SIGNAL_MAP, detach_process=False):
         logger.info("Switched into daemon context")
         while not killswitch.is_set():
             # find out my ipv4
             try:
-                myip_v4 = requests.get(web).text
-                logger.info(f"Successfully received a response from web: {myip_v4}")
-
-                if ipv4_changed := (myip_v4 != last_ipv4):
-                    logger.info(f"IPv4 changed {last_ipv4} -> {myip_v4}")
-                    last_ipv4 = myip_v4
-
+                registry.set_ipv4(requests.get(web).text)
             except requests.exceptions.ConnectionError as e:
                 logger.warning(f"Could no access {web}: {e}")
+                terminate(None, None)
+            except AddressValueError as e:
+                logger.warning(f"Returned malformed IPv4 {e}")
                 terminate(None, None)
 
             # find out my ipv6
             if web_v6:
                 try:
-                    myip_v6 = requests.get(web_v6).text
-                    logger.info(f"Successfully received a response from web: {myip_v6}")
-
-                    if ipv6_changed := (myip_v6 != last_ipv6):
-                        logger.info(f"IPv6 changed {last_ipv6} -> {myip_v6}")
-                        last_ipv6 = myip_v6
-
+                    registry.set_ipv6(requests.get(web_v6).text)
                 except requests.exceptions.ConnectionError as e:
                     logger.warning(f"Could no access {web_v6}: {e}")
                     terminate(None, None)
+                except AddressValueError as e:
+                    logger.warning(f"Returned malformed IPv4 {e}")
+                    terminate(None, None)
 
-            if ipv4_changed or ipv6_changed:
+            if registry.ipv4_changed or registry.ipv6_changed:
                 # build the url and the params
                 url = f"https://{ddns_server}/nic/update"
-                ipv4 = f"myip={myip_v4}"
-                last_ipv4 = myip_v4
-                ipv6 = f"myipv6={myip_v6}" if web_v6 else None
-                last_ipv6 = myip_v6
+
+                ipv4_query = f"myip={registry.ipv4}"
+                ipv6_query = f"myipv6={registry.ipv6}" if registry.ipv6_changed else ""
 
                 try:
-                    result = requests.get(f"{url}?{ipv4}&{ipv6 or ''}", auth=(login, password))
+                    if not dry_run:
+                        result = requests.get(f"{url}?{ipv4_query}&{ipv6_query}", auth=(login, password))
                     logger.info(f"Request successfully sent: {result.request.url}")
                     result.raise_for_status()
                 except requests.exceptions.RequestException as e:
                     logger.error(f"Encountered a problem setting the new IP: {e}")
             else:
-                logger.info(f"IPv4 {myip_v4} and IPv6 {myip_v6} did not change. Not updating the DYNDNS Endpoint")
+                logger.info(
+                    f"IPv4 {registry.ipv4} and IPv6 {registry.ipv6} did not change. Not updating the DYNDNS Endpoint")
 
             killswitch.wait(delay)
 
